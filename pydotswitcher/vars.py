f51e71aba35a716e7343c2eee4fb7cbab7e93c3c
Ehllay <max.pruther@gmail.com>
2023-09-24 16:34:35 +0100

    Implemented setup

--- pydotswitcher/vars.py
+++ pydotswitcher/vars.py
@@ -1,7 +1,7 @@
 import os
 
 # Versioning
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 # Directories
 HOME = os.path.expanduser("~")
--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
+# setup and packaging
+import sys
+import setuptools
+
 try:
     import pydotswitcher
 except ImportError:
     print("PDS requires python 3.11 or greater.")
+    sys.exit()
 
+README = open("README.md").read()
 VERSION = pydotswitcher.__version__
-print(VERSION)
+URL = "https://github.com/Ehllay/PyDotSwitcher"
+DOWNLOAD_URL = f"{URL}/archive/PDS_{VERSION}.tar.gz"
+
+setuptools.setup(
+    name="pydotswitcher",
+    version=VERSION,
+    author="Ehllay",
+    author_email="",
+    description="Switch up your dotfiles",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    keywords="dotfiles, backup, utility config ricing colorscheme linux python",
+    license="MIT",
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.11 ",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["dotswitch=pydotswitcher.__main__:main"]},
+    python_requires=">3.11",
+    # TODO: test_suite="tests",
+)
